78e997cd21a4a61ca1d050095da809e7175356e4
Anne Urai <anne.urai@gmail.com>
2015-12-10 18:27:36 +0100

    even better permtest

--- stats/permtest.m
+++ stats/permtest.m
@@ -2,7 +2,7 @@ function out = permtest(a,b,tail,nrand)
 % paired or one-sample permutation test
 
 if ~exist('b', 'var'),   b = zeros(size(a)); end
-if ~exist('nrand', 'var'), nrand = 1000; end
+if ~exist('nrand', 'var'), nrand = 10000; end
 if ~exist('tail', 'var'), tail = 0; end
 
 a = a(:);
