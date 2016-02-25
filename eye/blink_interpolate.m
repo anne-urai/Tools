8796b35d6e42ca1ea266c0f929ea39caef497cc8
Anne Urai <anne.urai@gmail.com>
2016-02-25 09:28:24 +0100

    improved plotting

--- eye/blink_interpolate.m
+++ eye/blink_interpolate.m
@@ -1,6 +1,6 @@
 function [newpupil, newblinksmp] = blink_interpolate(data, blinksmp, plotme)
 % interpolates blinks and missing data
-% Anne Urai, 2015
+% Anne Urai, 2016
 
 % get the stuff we need
 dat.time        = data.time{1};
@@ -8,9 +8,7 @@ dat.pupil       = data.trial{1}(~cellfun(@isempty, strfind(data.label, 'EYEPUPIL
 dat.gazex       = data.trial{1}(~cellfun(@isempty, strfind(data.label, 'EYEH')),:);
 dat.gazey       = data.trial{1}(~cellfun(@isempty, strfind(data.label, 'EYEV')),:);
 padding         = 0.150; % how long before and after do we want to pad?
-
-% get sample idx from asc
-dat.blinksmp = blinksmp;
+dat.blinksmp    = blinksmp; % get sample idx from asc
 
 % initialize settings
 if ~exist('plotme', 'var'); plotme = true; end % plot all this stuff
@@ -23,7 +21,7 @@ if ~isempty(blinksmp),
     % ====================================================== %
     
     if plotme,
-        clf;  sp1 = subplot(511); plot(dat.time,dat.pupil, 'color', [0.5 0.5 0.5]);
+        clf;  sp1 = subplot(411); plot(dat.time,dat.pupil, 'color', [0.5 0.5 0.5]);
         axis tight; box off; ylabel('Raw');
         set(gca, 'xtick', []);
     end
@@ -53,7 +51,8 @@ if ~isempty(blinksmp),
     end
     
     % also set the pupil to zero when there were missing data
-    dat.pupil(dat.pupil<nanmedian(dat.pupil)-2*nanstd(dat.pupil)) = nan;
+    dat.pupil(dat.pupil < nanmedian(dat.pupil)-3*nanstd(dat.pupil)) = nan;
+    dat.pupil(dat.pupil > nanmedian(dat.pupil)+3*nanstd(dat.pupil)) = nan;
     
     % interpolate linearly
     dat.pupil(isnan(dat.pupil)) = interp1(find(~isnan(dat.pupil)), ...
@@ -65,10 +64,10 @@ if ~isempty(blinksmp),
     % dat.pupil(end-edgepad*data.fsample : end)   = NaN;
     
     % also extrapolate ends
-    % dat.pupil(isnan(dat.pupil)) = interp1(find(~isnan(dat.pupil)), ...
-    %    dat.pupil(~isnan(dat.pupil)), find(isnan(dat.pupil)), 'nearest', 'extrap');
+    dat.pupil(isnan(dat.pupil)) = interp1(find(~isnan(dat.pupil)), ...
+        dat.pupil(~isnan(dat.pupil)), find(isnan(dat.pupil)), 'nearest', 'extrap');
     
-    if plotme, sp2 = subplot(511); hold on;
+    if plotme, sp2 = subplot(411); hold on;
         % show how well this worked
         plot(dat.time, dat.pupil, 'b');
         axis tight; box off; ylabel('Interp');
@@ -80,20 +79,27 @@ end
 % STEP 2: INTERPOLATE PEAK-DETECTED BLINKS
 % ====================================================== %
 
+assert(~any(isnan(dat.pupil)));
 win             = hanning(11);
 pupildatsmooth  = filter2(win.',dat.pupil,'same');
 
 dat.pupildiff   = diff(pupildatsmooth) - mean(diff(pupildatsmooth)) / std(diff(pupildatsmooth));
 [peaks, loc]    = findpeaks(abs(dat.pupildiff), 'minpeakheight', 3*std(dat.pupildiff), 'minpeakdistance', 0.5*data.fsample);
 
-if plotme, sp3 = subplot(512);
+if plotme, sp2 = subplot(412);
     plot(dat.time(2:end), dat.pupildiff);
     hold on; plot(dat.time(loc), peaks, 'o');
-    axis tight; box off; ylabel('Peak detect');
+    axis tight; box off; ylabel('Peaks');
     set(gca, 'xtick', []);
 end
 
 if ~isempty(peaks),
+    
+    if plotme, sp3 = subplot(413);
+        plot(dat.time, dat.pupil, 'color', [0.5 0.5 0.5]);
+        hold on; 
+    end
+    
     % convert peaks into blinksmp
     newblinksmp = nan(length(peaks), 2);
     for p = 1:length(peaks),
@@ -124,12 +130,7 @@ if ~isempty(peaks),
     % interpolate linearly
     dat.pupil(isnan(dat.pupil)) = interp1(find(~isnan(dat.pupil)), ...
         dat.pupil(~isnan(dat.pupil)), find(isnan(dat.pupil)), 'linear');
-  
-    if plotme,
-        sp4 = subplot(514); plot(dat.time, dat.pupil);
-        axis tight; box off; ylabel('Clean');
-        set(gca, 'xtick', []);
-    end
+    
 end
 
 % remove remaining nans (probably at the end)
@@ -141,15 +142,12 @@ newpupil = dat.pupil;
 
 % link axes
 if plotme,
-    try
-        linkaxes([sp1 sp2 sp3 sp4], 'x');
-        set([sp1 sp2 sp3 sp4], 'tickdir', 'out');
-    catch
-        linkaxes([sp1 sp2 sp3], 'x');
-        set([sp1 sp2 sp3], 'tickdir', 'out');
-        
-    end
-    xlim([-10 dat.time(end)+10]);
+    plot(dat.time, dat.pupil, 'b'); 
+    ylim([min(dat.pupil)*0.9 max(dat.pupil)*1.1]);
+    box off; ylabel('Clean');
+    linkaxes([sp1 sp2 sp3], 'x');
+    set([sp1 sp2 sp3], 'tickdir', 'out');
+    xlim([dat.time(1)-10 dat.time(end)+10]);
 end
 
 end
