b33b84e4a3ab51262aa3f7055e57f1a96e777bfc
Anne Urai <anne.urai@gmail.com>
2017-01-17 21:33:59 +0100

    boundedline to klimag

--- divideintobins.m
+++ divideintobins.m
@@ -26,8 +26,12 @@ else
     binIdx = discretize(x, [-inf quantile(x, nbins-1) inf]);
 end
 
+try
 % get the summary measure for each bin
 binnedx = splitapply(summaryFunc, x, binIdx);
+catch
+  assert(1==0);
+end
 binnedy = splitapply(summaryFunc, y, binIdx);
 
 % also get the distribution within each bin
@@ -36,7 +40,7 @@ stdy = splitapply(distFun, y, binIdx);
 
 % do some statistics
 if ~isempty(corrtype),
-    % on binned or non-binned data? binning will change rho, but not really 
+    % on binned or non-binned data? binning will change rho, but not really
     % the p-value of the correlation
     [rho, pval] = corr(x(:), y(:), 'type', corrtype);
 else
